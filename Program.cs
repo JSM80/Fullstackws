8a1c0de241cdb992832bccdc6dbd9199b8894865
JSM80 <mail@moellers-hule.dk>
2024-02-08 21:42:34 +0100

    video 1 done

--- Program.cs
+++ Program.cs
@@ -1,44 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+using Fleck;
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var server = new WebSocketServer("ws://0.0.0.0:8181");
+
+var wsConnections = new List<IWebSocketConnection>();
+
+server.Start(ws =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    ws.OnOpen = () =>
+    {
+        wsConnections.Add(ws);
 
-app.UseHttpsRedirection();
+    };
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+    ws.OnMessage = message =>
+    {
+        foreach (var webSocketConnection in wsConnections)
+        {
+            webSocketConnection.Send(message);
+        }
+     
+    };
+});
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
 
-app.Run();
+WebApplication.CreateBuilder(args).Build().Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+
+
+
